62c27eb0e69a27f740dc83c0e633951021c6c13a
Youngj7449 <youngj@faytechcc.edu>
2017-06-10 16:22:43 -0400

    Add files via upload
    This program calculates the distance traveled at 70 mph in various hours.

---
+++ M2HW1_DistanceTraveled_Young.py
@@ -0,0 +1,11 @@
+# Calculates distance that a car travels based on speed
+# 6/11/2017
+# CTI-110 M2HW1 - Distance Traveled
+# Jessica Young
+#
+
+speed = 70
+print( 'In 6 hours, the distance the car will travel is ' + str ((speed*6)) + ' miles.')
+print( 'In 10 hours, the distance the car will travel is ' + str ((speed*10)) + ' miles.')
+print( 'In 15 hours, the distance the car will travel is ' + str ((speed*15)) + ' miles.')
+
