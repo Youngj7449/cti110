2685218ca45de936607d281b2d84b0575a71e80a
Youngj7449 <youngj@faytechcc.edu>
2017-07-23 15:18:45 -0400

    Add files via upload
    Reads numbers

---
+++ M6HW2_RandomNumberFileReader_Young.py
@@ -0,0 +1,17 @@
+#Generates Random Numbers
+#7/23/17
+#CTI-110 M6HW2 - Random Number FIle Reader
+#Jessica Young
+#
+
+
+def main():
+
+    randfile = open("Randomnm.txt", "r" )
+
+file_contents = infile.read()
+infile.close()
+
+print(file_contents)
+
+main()
